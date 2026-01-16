0a1c78b9ae8dc3701827e8df28ad5c9baa7ff2d8
KonyaGellert <konyag@kkszki.hu>
2026-01-16 10:03:30 +0100

    osztály

--- Allatkert/Program.cs
+++ Allatkert/Program.cs
@@ -6,10 +6,36 @@ using System.Threading.Tasks;
 
 namespace Allatkert
 {
+    public class Allat
+    {
+        public string Nev;
+        public string Faj;
+        public int SzuletesiEv;
+        public string Elohely;
+
+        public Allat(string nev, string faj, int szuletesiEv, string elohely)
+        {
+            Nev = nev;
+            Faj = faj;
+            SzuletesiEv = szuletesiEv;
+            Elohely = elohely;
+        }
+
+        public override string ToString()
+        {
+            return "Az állat neve:"+ Nev + ",Faja:"+ Faj + ",Szuletesiéve:"+ SzuletesiEv + ",Élőhelye" +Elohely ;
+        }
+        public int hanyeves()
+        {
+            return DateTime.Now.Year-SzuletesiEv;
+        }
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
+            
+
         }
     }
 }
