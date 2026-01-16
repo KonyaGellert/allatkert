ab5aed2f41bd4f9293a0c26c988c93bc7c28101b
KonyaGellert <konyag@kkszki.hu>
2026-01-16 10:08:49 +0100

    Állat Lista feltöltése

--- Allatkert/Program.cs
+++ Allatkert/Program.cs
@@ -34,7 +34,14 @@ namespace Allatkert
     {
         static void Main(string[] args)
         {
-            
+            //5 állat listába
+            List<Allat> allatkert = new List<Allat>();
+            allatkert.Add(new Allat("Péter", "Oroszlán", 2015, "szavanna"));
+            allatkert.Add(new Allat("Jakab", "Elefánt", 2010, "szavanna"));
+            allatkert.Add(new Allat("Zoltán", "Zebra", 2018, "szavanna"));
+            allatkert.Add(new Allat("Bpapi", "Papagáj", 2012, "dzsungel"));
+            allatkert.Add(new Allat("Attila", "Pingvin", 2016, "sarkvidék"));
+
 
         }
     }
